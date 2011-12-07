e57b2e98a7d2d515730ec0841536136f34d39cd7
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-12-07 07:21:29 +0000

    Adding MyGuestbook

---
+++ myguestbook/docs/readme.txt
@@ -0,0 +1,99 @@
+NARGA'S GUESTBOOK MODULES FOR XOOPS PORTAL 2.X
+==============================================
+
+*********************************** IMPORTANT NOTES *****************************************
+        This software is freeware. You don't have the permission to sell this product to anyone! The copyright in the footer must seeable (only not if you have purchased a Non-Copyright-Version). 
+        Also if you changed the Code or use your own Hackz! With remove of the Copyright you agree that you must buy the Non-Copyright-Version. 
+        [:: Narga Laboratory ::] don't liable for damages in all ways. 
+        Furthermore we must clue that [:: Narga Laboratory ::] and his employees are not responsible for any textcontents.
+*********************************************************************************************
+
+Contents:
+--------
+
+0. Announcement.
+1. Availability
+2. Requirements and feature
+3. History
+4. Step by step to installation
+5. Support and Documentation
+6. Author & Copyright
+
+
+Announcement:
+-------------
+
+        After 3 months of work and 4 release candidate versions,the [:: Narrga Staffs ::] are pleased to announce the availability of Narga's Guestbook modules for Xoops Portal 2.x !
+
+Availability
+------------
+
+        This software is available under the GNU General Public License V2.0.
+        You can get the newest version at http://www.narga.tk/
+        Available file formats are: *.zip, *.tar.gz and *.rar.
+
+        If you install Vietnamese language package on your system, it's recommended to subscribe to the news mailing list by adding your address under [http://www.narga.tk]
+
+        This way, you will be informed of new updates and security fixes. It is a read only list, and traffic is not greater than a few mail every year.
+
+Requirements:
+-------------
+        Xoops Portal 2.x forum installed.
+        PHP3 (>= 3.0.8) or PHP4
+        MySQL (tested with 3.21.x, 3.22.x, 3.23.x and 4.0.x) a web-browser (doh!)
+    
+        Feature :
+                - Allow bbcode and smile.
+                - Show YIM, AIM, MSN, ICQ, Email, Homepage in each entry
+                - Uses Smarty Templates in the Xoops platform
+
+History:
+--------
+
+        - Read the Changes-log.txt file to get detail
+
+Step by step to installation :
+-----------------------------
+
+        - unzip file to html/modules/ directory the upload to your server
+        - go to the admin control panel to install this modules
+        - DO NOT remove the Copyright note ! Thanks !
+
+	If you want a "Sign" link at main menu you can edit the xoops_version.php as:
+	add this line
+	$modversion['sub'][1]['name'] = _NAR_SIGNGUESTBOOK;
+	$modversion['sub'][1]['url'] = "sign.php";
+	below
+	// Menu
+	$modversion['hasMain'] = 1;
+
+Support and Documentation
+-------------------------
+
+        The documentation is included in the software package as text and HTML file.
+        All the error you have can solve on [:: Narga Vault::] http://www.narga.tk or contact me at webmaster@narga.tk .
+
+Author & Copyright
+------------------
+
+    Copyright (C) 2002-2003 Nguyen Dinh Quan - Narga's Guestbook is a produce
+    of [:: Narga Laboratory ::] - http://www.nargalab.info
+    <webmaster_at_narga.tk>
+    webmaster@narga.tk
+    http://www.narga.tk
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License (include in this
+    folder) as published by the Free Software Foundation; either version
+    2 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+Last update : 8/27/2004 11:25 PM by Nguyen Dinh Quan.
---
+++ myguestbook/include/functions.php
@@ -0,0 +1,141 @@
+<?php
+###############################################################################
+#                  Narga's Guestbook v.1.0 for Xoops 2.x                      #
+#             Writen by  Nguyen Dinh Quan (webmaster@narga.tk)                #
+#      .: Narga Vault :-: The Land Of Dreams :.(http://www.narga.tk)          #
+#   ------------------------------------------------------------------------- #
+#                                                                             #
+#   ------------------------------------------------------------------------- #
+#   This program is free software; you can redistribute it and/or modify      #
+#   it under the terms of the GNU General Public License as published by      #
+#   the Free Software Foundation; either version 2 of the License, or         #
+#   (at your option) any later version.                                       #
+#                                                                             #
+#   This program is distributed in the hope that it will be useful,           #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#   GNU General Public License for more details.                              #
+#                                                                             #
+#   You should have received a copy of the GNU General Public License         #
+#   along with this program; if not, write to the Free Software               #
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA  #
+#   ------------------------------------------------------------------------  #
+###############################################################################
+// $Id: functions.php,v 1.2 Date: 22/06/2003, Author: Nguyen Dinh Quan Exp $
+include_once XOOPS_ROOT_PATH."/class/xoopsobject.php";
+
+class MyGuestbookSystem extends XoopsObject
+{
+	var $db;
+
+    // constructor
+	function MyGuestbookSystem($id=null)
+	{
+		$this->db =& Database::getInstance();
+		$this->initVar("id", XOBJ_DTYPE_INT, null, false);
+		$this->initVar("name", XOBJ_DTYPE_TXTBOX, null, true);
+		$this->initVar("title", XOBJ_DTYPE_TXTBOX, null, true);
+		$this->initVar("message", XOBJ_DTYPE_TXTAREA, null, true);
+		$this->initVar("note", XOBJ_DTYPE_TXTBOX, null, true);
+		$this->initVar("time", XOBJ_DTYPE_STIME, null, true);
+		$this->initVar("email", XOBJ_DTYPE_EMAIL, null, true);
+		$this->initVar("url", XOBJ_DTYPE_URL, null, true);
+		$this->initVar("ip", XOBJ_DTYPE_TXTBOX, null, true);
+		$this->initVar("gender", XOBJ_DTYPE_INT, null, true, 1);
+		$this->initVar("icq", XOBJ_DTYPE_INT, null, true, 15);
+		$this->initVar("yim", XOBJ_DTYPE_TXTBOX, null, true);
+		$this->initVar("aim", XOBJ_DTYPE_TXTBOX, null, true);
+		$this->initVar("msn", XOBJ_DTYPE_TXTBOX, null, true);
+		$this->initVar("location", XOBJ_DTYPE_TXTBOX, null, true);
+		$this->initVar("company", XOBJ_DTYPE_TXTBOX, null, true);
+		if ( !empty($id) ) {
+			if ( is_array($id) ) {
+				$this->assignVars($id);
+			} else {
+				$this->load(intval($id));
+			}
+		}
+	}
+
+	function load($id)
+	{
+		$sql = "SELECT * FROM ".$this->db->prefix("myguestbook")." WHERE id=$id and time > 0";
+		$myrow = $this->db->fetchArray($this->db->query($sql));
+		$this->assignVars($myrow);
+	}
+function genderMySelBox($title,$order="",$preset_id=0, $none=0, $sel_name="", $onchange="")
+	{
+		if ( $sel_name == "" ) {
+			$sel_name = $this->id;
+		}
+		$myts =& MyTextSanitizer::getInstance();
+		echo "<select name='".$sel_name."'";
+		if ( $onchange != "" ) {
+			echo " onchange='".$onchange."'";
+		}
+		echo ">\n";
+		$sql = "SELECT ".$this->id.", ".$title." FROM ".$this->table." WHERE ".$this->pid."=0";
+		if ( $order != "" ) {
+			$sql .= " ORDER BY $order";
+		}
+		$result = $this->db->query($sql);
+		if ( $none ) {
+			echo "<option value='0'>----</option>\n";
+		}
+		while ( list($catid, $name) = $this->db->fetchRow($result) ) {
+			$sel = "";
+			if ( $catid == $preset_id ) {
+				$sel = " selected='selected'";
+			}
+			echo "<option value='$catid'$sel>$name</option>\n";
+			$sel = "";
+			$arr = $this->getChildTreeArray($catid);
+			foreach ( $arr as $option ) {
+				$option['prefix'] = str_replace(".","--",$option['prefix']);
+				$catpath = $option['prefix']."&nbsp;".$myts->makeTboxData4Show($option[$title]);
+				if ( $option[$this->id] == $preset_id ) {
+					$sel = " selected='selected'";
+				}
+				echo "<option value='".$option[$this->id]."'$sel>$catpath</option>\n";
+				$sel = "";
+			}
+		}
+		echo "</select>\n";
+	}
+	
+	function getAllMessages($criteria=array(), $asobject=false, $limit=0, $start=0, $order="")
+	{
+		$db =& Database::getInstance();
+		$ret = array();
+		$where_query = "";
+		if ( is_array($criteria) && count($criteria) > 0 ) {
+			$where_query = " WHERE";
+			foreach ( $criteria as $c ) {
+				$where_query .= " $c AND";
+			}
+			$where_query = substr($where_query, 0, -4);
+		} elseif ( !is_array($criteria) ) {
+			$where_query = " WHERE ".$criteria;
+		}
+		if ( !$asobject ) {
+			$sql = "SELECT id FROM ".$db->prefix("myguestbook")."".$where_query." ORDER BY time $order";
+			$result = $db->query($sql,intval($limit),intval($start));
+			while ( $myrow = $db->fetchArray($result) ) {
+				$ret[] = $myrow['id'];
+			}
+		} else {
+			$sql = "SELECT * FROM ".$db->prefix("myguestbook")."".$where_query." ORDER BY time $order";
+			$result = $db->query($sql,intval($limit),intval($start));
+			while ( $myrow = $db->fetchArray($result) ) {
+				$ret[] = new MyGuestbookSystem($myrow);
+			}
+				if ( $order != "" ) {
+			$sql .= " ORDER BY $order";
+		}
+
+		}
+		return $ret;
+	}
+
+}
+?>
